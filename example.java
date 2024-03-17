15868249aace0c2cc6bf3e9d9f1087ecb28708d8
unknown <ys9497339@gmail.com>
2024-03-17 10:08:26 +0530

    Changed at line 3 to 8

--- example.java
+++ example.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class example {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome");
+		System.out.println("Enter two numbers : ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println("Addition : "+(a+b));
 	}
 }
